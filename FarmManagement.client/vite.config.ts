5541035566cac454e3ee9b95b909801f1a9b0d02
Micheal Nkuna <102879588+nkunasap@users.noreply.github.com>
2026-09-08 06:16:59 +0000

    3

--- FarmManagement.client/vite.config.ts
+++ FarmManagement.client/vite.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const API_TARGET = 'http://localhost:5226'
+
+
 export default defineConfig({
   plugins: [react()],
   server: {
